ab45fb7ffc6e31850ebaa64906204f98525cf18d
AlexSolizDuran <solizelex1999@gmail.com>
2025-09-27 21:49:50 -0400

    seed2

--- condominio/settings.py
+++ condominio/settings.py
@@ -106,6 +106,15 @@ WSGI_APPLICATION = 'condominio.wsgi.application'
 
 DATABASES = {
     'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'condominio',
         'USER': 'user0',
@@ -114,8 +123,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
